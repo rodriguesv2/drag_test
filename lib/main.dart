cb1dcf015b8c2a11de1b9a21fbe5720103a933c2
Rubens Rodrigues <rubens.rodrigues@mobile2you.com.br>
2022-05-02 12:11:53 -0300

    feat: (Added the drag function to list's cells)

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: "Teste Drag"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final list = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet venenatis magna, et fringilla ante.",
+    "Etiam ut felis in metus placerat dapibus. Vestibulum fringilla iaculis dui, eu iaculis nulla blandit vel.",
+    "Aenean eget tellus vitae sem euismod feugiat. Phasellus lobortis eleifend urna, quis consequat augue semper a. Etiam condimentum cursus venenatis. Mauris id suscipit enim. Phasellus pellentesque luctus condimentum.",
+    "Nullam ut lacus tortor. Cras non erat purus. In felis justo, tristique at accumsan nec, lobortis non odio.",
+    "Donec fringilla ante mi, a faucibus eros hendrerit non. Aenean elementum enim sed nibh aliquet venenatis. In egestas dignissim nisi lobortis viverra. Cras et quam nunc.",
+    "Quisque varius, orci ac blandit commodo, metus dui congue lacus, quis rhoncus quam sapien eu augue. In eu ex et nisl pellentesque congue a eget nisl.",
+    "Vestibulum convallis erat ac nunc aliquet, in mattis dolor aliquet. Donec id suscipit nunc, eget gravida nunc. ",
+    "Integer ultrices quam mi, ut feugiat felis mattis id. Donec ornare vehicula efficitur. Sed lectus erat, laoreet quis leo vel, volutpat tempor ante.",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SizedBox(
+        width: 400,
+        child: ListView(
+          children: [
+            ...list.map((text) {
+              return Draggable(
+                child: _textCard(text),
+                feedback: _textCard(
+                  text,
+                  backgroundColor: const Color(0xAAFF6E40),
+                ),
+                childWhenDragging: _textCard(
+                  text,
+                  shouldHide: true,
+                ),
+              );
+            }).toList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textCard(
+    String text, {
+    Color backgroundColor = const Color(0xFFFF6E40),
+    bool shouldHide = false,
+  }) {
+    return SizedBox(
+      width: 400,
+      child: Card(
+        color: shouldHide ? Colors.transparent : backgroundColor,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.headline6?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: shouldHide ? Colors.transparent : Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
